716e919399869e4fb4871e63cfba4493002846ff
sumin <lsum319@gmail.com>
2026-07-10 00:01:35 +0900

    initial commit

---
+++ src/main/java/com/sumin/knowledgearchive/KnowledgeArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.sumin.knowledgearchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumin/knowledgearchive/KnowledgeArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumin.knowledgearchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
